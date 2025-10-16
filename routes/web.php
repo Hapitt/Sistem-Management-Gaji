ee3bfec75ddd1f850ba6f8e09a0cfd75f8abe00c
Hapitt <muhammadhafidziakhan@gmail.com>
2025-10-16 11:17:53 +0700

    update perubahan kecil

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,35 @@
 <?php
 
-use App\Http\Controllers\GajiController;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\RatingController;
-use App\Http\Controllers\KaryawanController;
-use App\Http\Controllers\JabatanController;
-use App\Http\Controllers\LemburController;
-use App\Http\Controllers\ProfileController;
-
-
 use Illuminate\Support\Facades\Route;
-
-use function Ramsey\Uuid\v1;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\{
+    DashboardController,
+    ProfileController,
+    KaryawanController,
+    JabatanController,
+    RatingController,
+    LemburController,
+    GajiController
+};
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
 Auth::routes();
-Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
 
+    Route::get('gaji/calculate', [GajiController::class, 'calculate'])->name('gaji.calculate');
+    Route::get('/gaji/cetak/{id}', [GajiController::class, 'cetak'])->name('gaji.cetak');
 
-Route::resource('karyawan', KaryawanController::class);
-Route::resource('jabatan', JabatanController::class);
-Route::resource('rating', RatingController::class);
-Route::resource('lembur', LemburController::class);
-Route::get('gaji/calculate', [GajiController::class, 'calculate'])->name('gaji.calculate');
-Route::get('/gaji/cetak/{id}', [GajiController::class, 'cetak'])->name('gaji.cetak');
-
-Route::resource('gaji', GajiController::class);
-
+    Route::resource('karyawan', KaryawanController::class);
+    Route::resource('jabatan', JabatanController::class);
+    Route::resource('rating', RatingController::class);
+    Route::resource('lembur', LemburController::class);
+    Route::resource('gaji', GajiController::class);
+});
